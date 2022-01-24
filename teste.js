1b6dc63d6b80f4eb6ee44cc01373237a30a7d1f9
A46628 <A46628@alunos.isel.pt>
2022-01-24 16:50:58 +0000

    teste2

--- teste.js
+++ teste.js
@@ -1,1 +1,23 @@
 console.log("verificado com sucesso")
+
+
+
+const TASKS_DATA_HOST = 'localhost:9200'
+const DEFAULT_PORT = '8080'
+
+
+const path = require('path')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.get('/', tasksApi)
+
+app.listen(process.argv[2] || 8080 , () => console.log("Listening"))
+
+
+
+function tasksApi(req, resp) {
+    resp.send("fe no pai")
+}
